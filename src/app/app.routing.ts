af3a21323d2a8b2144be808d9eb9bb44c9e70aa8
erikzaksf <erik.zak.sf@gmail.com>
2017-11-03 11:08:11 -0700

    add navbar and routing for about page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,19 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { AboutComponent }   from './about/about.component';
+
 
 const appRoutes: Routes = [
   {
     path: '',
    component: LandingComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
