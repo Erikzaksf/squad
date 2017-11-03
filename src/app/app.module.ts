c7f73409e5a9cff535b2f17c6d9025a90ad91c23
erikzaksf <erik.zak.sf@gmail.com>
2017-11-03 11:52:21 -0700

    add clarity css framework for advanced styling and tables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { ClarityModule } from "clarity-angular";
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { routing } from './app.routing';
